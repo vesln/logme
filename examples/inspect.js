47aa52739df1e6981b8f18debaeaaa3a53e079a5
Veselin Todorov <veselin@veselin.bg>
2011-12-10 16:51:27 +0200

    Adding inspect for better debugging

---
+++ examples/inspect.js
@@ -0,0 +1,13 @@
+/**
+ * Logme - Minimalistic logging.
+ * 
+ * Author: Veselin Todorov <hi@vesln.com>
+ * Licensed under the MIT License.
+ */
+
+var logme = require('../');
+
+/**
+ * Like sexy var_dump.
+ */
+logme.inspect({ foo: 'bar' });
