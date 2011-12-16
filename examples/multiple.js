056967d320ffe73cc1866ec4a78c805fab168d7b
Veselin Todorov <veselin@veselin.bg>
2011-12-16 18:46:17 +0200

    Adding support for logging multiple values at once

---
+++ examples/multiple.js
@@ -0,0 +1,10 @@
+/**
+ * Logme - Minimalistic logging.
+ * 
+ * Author: Veselin Todorov <hi@vesln.com>
+ * Licensed under the MIT License.
+ */
+
+var logme = require('../');
+logme.error('That', 'is', 'cool');
+logme.error('That is so awesome');
